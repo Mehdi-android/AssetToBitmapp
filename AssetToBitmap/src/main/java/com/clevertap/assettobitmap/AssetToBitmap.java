b2272285f361c9d7417b443c4dab3fbc646867c2
user <mehdi@collabu.com>
2021-03-20 20:03:08 +0530

    Initial commit

---
+++ AssetToBitmap/src/main/java/com/clevertap/assettobitmap/AssetToBitmap.java
@@ -0,0 +1,24 @@
+package com.clevertap.assettobitmap;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AssetToBitmap {
+    public static Bitmap getBitmapFromAsset(Context context, String strName) {
+        AssetManager assetManager = context.getAssets();
+        InputStream istr;
+        Bitmap bitmap = null;
+        try {
+            istr = assetManager.open(strName);
+            bitmap = BitmapFactory.decodeStream(istr);
+        } catch (IOException e) {
+            return null;
+        }
+        return bitmap;
+    }
+}
